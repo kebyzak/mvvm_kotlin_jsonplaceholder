2de21acb22760740331870253765ae91b1fe7993
Kazybek <kazbek.auez@gmail.com>
2023-04-16 19:54:08 +0600

    second commit

---
+++ app/src/main/java/kz/tutorial/jsonplaceholdertypicode/presentation/posts/PostsViewModel.kt
@@ -0,0 +1,27 @@
+package kz.tutorial.jsonplaceholdertypicode.presentation.posts
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kz.tutorial.jsonplaceholdertypicode.domain.use_cases.GetPostsUseCase
+import kz.tutorial.jsonplaceholdertypicode.domain.models.Post
+
+class PostsViewModel(private val getPostsUseCase: GetPostsUseCase) : ViewModel() {
+
+    private val _postsLiveData: MutableLiveData<List<Post>> = MutableLiveData()
+    val postsLiveData: LiveData<List<Post>> = _postsLiveData
+
+    init {
+        getPosts()
+    }
+
+    private fun getPosts() {
+        viewModelScope.launch {
+            val posts = getPostsUseCase()
+            _postsLiveData.postValue(posts)
+        }
+    }
+
+}
